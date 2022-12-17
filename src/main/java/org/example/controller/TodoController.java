8ac7b6ab1ef29e1fbddbc44a4229281700abec1c
Anjaehyun <lvet0330@gmail.com>
2022-12-17 18:00:57 +0900

    # todo boot

--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -1,9 +1,12 @@
 package org.example.controller;
 
 import lombok.AllArgsConstructor;
+import org.example.model.TodoEntity;
+import org.example.model.TodoRequest;
 import org.example.model.TodoResponse;
 import org.example.service.TodoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +20,21 @@ public class TodoController {
     private final TodoService service;
 
     @PostMapping
-    public ResponseEntity<TodoResponse> create(){
+    public ResponseEntity<TodoResponse> create(@RequestBody TodoRequest request){
         System.out.println("CREATE");
+
+//        if(ObjectUtils.isEmpty(request.getTitle()))
+//            return ResponseEntity.badRequest().build();
+//
+//        if(ObjectUtils.isEmpty(request.getOrder()))
+//            request.setOrder(0L);
+//
+//        if(ObjectUtils.isEmpty(request.getComplected()))
+//            request.setComplected(false);
+//
+//        TodoEntity result = this.service.add(request);
+//
+//        return ResponseEntity.ok(new TodoResponse(result));
         return null;
     }
 
@@ -34,13 +50,13 @@ public class TodoController {
         return null;
     }
 
-    @PatchMapping
+    @PatchMapping("{id}")
     public ResponseEntity<TodoResponse> update(){
         System.out.println("UPDATE");
         return null;
     }
 
-    @DeleteMapping
+    @DeleteMapping("{id}")
     public ResponseEntity<?> deleteOne(){
         System.out.println("DELETE");
         return null;
